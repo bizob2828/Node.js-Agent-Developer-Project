b9c02dcd525f76b73851ecf1a3a481932b2dd7c0
javon <kidc12>
2019-11-25 13:52:16 -0500

    progress

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var uuid = require('node-uuid')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+logger.token('id', function getId (req) {
+  return req.id
+})
 
 var app = express();
 
@@ -14,7 +19,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(logger('DATA: :method :url :status :res[content-length] Request-Time: :response-time ms'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,6 +32,9 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
+
 
 // error handler
 app.use(function(err, req, res, next) {

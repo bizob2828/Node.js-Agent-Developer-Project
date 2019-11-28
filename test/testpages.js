04e95784d1a922d975e592ad0ae498b8509f9a94
kidc12 <kidc12@gmail.com>
2019-11-27 23:39:05 -0500

    unit test update

--- test/testpages.js
+++ test/testpages.js
@@ -1,27 +1,42 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+
+should = chai.should();
+
+chai.use(chaiHttp);
+
+let server = require('../app');
+
 describe('Status and content', function() {
     describe ('Main page', function() {
-        it('status', function(done){
-            request('http://localhost:3000/', function(error, response, body) {
-                //expect(response.statusCode).to.equal(200);
-                done();
-            });
-        });
+        it('status', (done) => {
+            chai.request(server)
+              .get('/')
+              .end((err, res) => {
+                (res).should.have.status(200);
+                    done();
+                 });
+              });
 
-        it('content', function(done) {
-            request('http://localhost:3000/' , function(error, response, body) {
-                expect(body).to.equal('Hello World');
+        it('content', (done) => {
+            chai.request(server) 
+            .get('/')
+            .end((err, res) => {
+                (res.body).should.be.a('object');
                 done();
             });
         });
     });
 
     describe ('About page', function() {
-        it('status', function(done){
-            request('http://localhost:3000/about', function(error, response, body) {
-                //expect(response.statusCode).to.equal(404);
+        it('status', (done) => {
+            chai.request(server)
+            .get('/about')
+            .end((err, res) => {
+                (res).should.have.status(404);
                 done();
             });
         });
